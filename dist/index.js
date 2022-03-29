4805b995f957958f97eb204ad1fd2c6999ed03da
letterpaulsand <letterpaulsand@gmail.com>
2022-03-29 19:52:49 +0800

    ok

--- dist/index.js
+++ dist/index.js
@@ -1,27 +1,44 @@
 "use strict";
+/**
+ * @version 1.0.0
+ * @copyright MIT
+ * @author paul
+ */
+// There is the config data below
+//--------- CONFIG ---------
+const BACKEND_URL = 'https://line-new.vercel.app';
+// DON'T ADD " / " AFTER THE URL
+//--------- CONFIG ---------
 let message = document.getElementById('message');
 let stickerPackageId = document.getElementById('sticker-package-id');
 let stickerId = document.getElementById('sticker-id');
 let send = document.getElementById('send');
+function changeTheResultBox(data) {
+}
 send.addEventListener('click', () => {
     let messageValue = message.value;
     let stickerPackageIdValue = stickerPackageId.value;
     let stickerIdValue = stickerId.value;
-    let obj = {};
+    // let SendToBackend: Post = {
+    //     message: ''
+    // }
     if (messageValue) {
         alert('ok');
     }
     else {
         alert('You have to type something!');
     }
-    console.log(obj);
-    fetch('http://localhost:5000/send', {
+    fetch(`${BACKEND_URL}/send`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         },
         body: `message=${messageValue}&stickerId=${stickerIdValue}&stickerPackageId=${stickerPackageIdValue}`
+    }).then(res => {
+        return res.json();
+    }).then(data => {
+        // changeTheResultBox(data)
+        console.log(data);
     });
-    console.log(messageValue, stickerPackageIdValue, stickerIdValue);
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
+/**
+ * @version 1.0.0
+ * @copyright MIT
+ * @author paul
+ */
+
+// There is the config data below
+
+//--------- CONFIG ---------
+
+
+const BACKEND_URL = 'https://line-new.vercel.app'
+// DON'T ADD " / " AFTER THE URL
+
+//--------- CONFIG ---------
+
 
 let message: any = document.getElementById('message')
 let stickerPackageId: any = document.getElementById('sticker-package-id')
 let stickerId: any = document.getElementById('sticker-id')
 let send: any = document.getElementById('send');
-
-
 
 interface Post {
     message?: string,
@@ -12,29 +26,42 @@ interface Post {
     stickerId?: number
 }
 
+interface Result{
+    status: number,
+    message: string,
+    source?: string 
+}
+
+function changeTheResultBox(data: Result){
+    
+}
+
 send.addEventListener('click', () => {
     let messageValue: string = message.value;
     let stickerPackageIdValue: number = stickerPackageId.value;
     let stickerIdValue: number = stickerId.value;
 
-    let obj: Post = {}
+    // let SendToBackend: Post = {
+    //     message: ''
+    // }
 
     if (messageValue) {
         alert('ok')
     } else {
         alert('You have to type something!')
     }
-
-    console.log(obj);
     
 
-    fetch('http://localhost:5000/send', {
+    fetch(`${BACKEND_URL}/send`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         },
         body: `message=${messageValue}&stickerId=${stickerIdValue}&stickerPackageId=${stickerPackageIdValue}`
+    }).then(res=>{
+        return res.json()
+    }).then(data=>{
+        // changeTheResultBox(data)
+        console.log(data);
     })
-
-    console.log(messageValue, stickerPackageIdValue, stickerIdValue);
 })
